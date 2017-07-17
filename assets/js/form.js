6f5e05088132acf45c5cf9e2861cfd4cf2cd06d6
ValHall <Valynhall@gmail.com>
2017-07-17 11:26:54 -0400

    new resume

---
+++ assets/js/form.js
@@ -0,0 +1,41 @@
+var formEmail = document.getElementById("formEmail"),
+    textArea = document.getElementById("formText"),
+    submitBtn = document.getElementById("submitBtn");
+
+textArea.addEventListener("focus", function(e) {
+  var targetValue = e.target.value;
+  if (targetValue === "What would you like to say?") {
+    e.target.value = "";
+  }
+  return;
+});
+
+textArea.addEventListener("blur", function(e) {
+  console.log(e)
+  var targetValue = e.target.value;
+  if (targetValue === "") {
+    e.target.value = "What would you like to say?";
+  }
+  return;
+});
+
+function emailConfirm() {
+  document.getElementById('goodEmail').style.display = 'block';
+  document.getElementById('badEmail').style.display = 'none';
+}
+
+function emailReject() {
+  document.getElementById('badEmail').style.display = 'block';
+  document.getElementById('goodEmail').style.display = 'none';
+}
+submitBtn.addEventListener("click", function(e) {
+  var isEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  e.preventDefault();
+
+  if (!isEmail.test(formEmail.value)) {
+      emailReject();
+  } else {
+    emailConfirm();
+  }
+
+});
