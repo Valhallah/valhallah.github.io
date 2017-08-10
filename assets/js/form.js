6666398cebc4e20032f7a90b96977eafd6df9b57
ValHall <Valynhall@gmail.com>
2017-08-10 15:15:28 -0400

    email form

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,22 +2,22 @@ var formEmail = document.getElementById("formEmail"),
     textArea = document.getElementById("formText"),
     submitBtn = document.getElementById("submitBtn");
 
-// textArea.addEventListener("focus", function(e) {
-//   var targetValue = e.target.value;
-//   if (targetValue === "What would you like to say?") {
-//     e.target.value = "";
-//   }
-//   return;
-// });
+textArea.addEventListener("focus", function(e) {
+  var targetValue = e.target.value;
+  if (targetValue === "What would you like to say?") {
+    e.target.value = "";
+  }
+  return;
+});
 
-// textArea.addEventListener("blur", function(e) {
-//   console.log(e)
-//   var targetValue = e.target.value;
-//   if (targetValue === "") {
-//     e.target.value = "What would you like to say?";
-//   }
-//   return;
-// });
+textArea.addEventListener("blur", function(e) {
+  console.log(e)
+  var targetValue = e.target.value;
+  if (targetValue === "") {
+    e.target.value = "What would you like to say?";
+  }
+  return;
+});
 
 function emailConfirm() {
   document.getElementById('goodEmail').style.display = 'block';
@@ -30,7 +30,7 @@ function emailReject() {
 }
 submitBtn.addEventListener("click", function(e) {
   var isEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  e.preventDefault();
+  // e.preventDefault();
 
   if (!isEmail.test(formEmail.value)) {
       emailReject();
@@ -41,7 +41,7 @@ submitBtn.addEventListener("click", function(e) {
     //     data: $(this).serialize(),
     //     dataType: "json"
     // });
-    e.preventDefault();
+    // e.preventDefault();
     emailConfirm();
   }
 });
